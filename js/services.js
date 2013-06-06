e9074a8a03e57dd305dd0bcc00e28afd4525de65
Olive Law <mariaolivia.law@gmail.com>
2013-06-06 11:32:29 +0800

    changes for promos

--- js/services.js
+++ js/services.js
@@ -1,25 +1,30 @@
 angular.module('mallServices', ['ngResource']).
-    factory('Mall', function($resource){
+factory('Mall', function($resource){
   return $resource("http://ayala360.net/api/v1/malls?callback=JSON_CALLBACK", {}, {
     query: {method:'GET', isArray:true}
   });
 }).
-    factory('Store', function($resource){
+factory('Store', function($resource){
   return $resource('data/:mallId/stores.json', {}, {
     query: {method:'GET', isArray:true}
   });
 }).
-    factory('Event', function($resource){
+factory('Event', function($resource){
   return $resource('data/:mallId/events.json', {}, {
     query: {method:'GET', isArray:true}
   });
 }).
-    factory('Food', function($resource){
+factory('Promos', function($resource){
+  return $resource('data/:mallId/promos.json', {}, {
+    query: {method:'GET', isArray:true}
+  });
+}).
+factory('Food', function($resource){
   return $resource('data/:mallId/foods.json', {}, {
     query: {method:'GET', isArray:true}
   });
 }).
-    factory('Category', function($resource){
+factory('Category', function($resource){
   return $resource('data/categories.json', {}, {
     query: {method:'GET', isArray:true}
   });
